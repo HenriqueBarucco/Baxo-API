c1213778462ee4767b67f2c5cdb7009fcb2a6254
Henrique Barucco <henrique_barucco@hotmail.com>
2024-07-30 14:01:24 -0300

    chore: create user entity

---
+++ src/main/kotlin/com/henriquebarucco/baxoapi/domain/user/entity/User.kt
@@ -0,0 +1,9 @@
+package com.henriquebarucco.baxoapi.domain.user.entity
+
+import java.util.UUID
+
+data class User(
+    val id: UUID,
+    val name: String,
+    val phone: String,
+)
---
+++ src/main/kotlin/com/henriquebarucco/baxoapi/domain/user/interactor/CreateUserInteractor.kt
@@ -0,0 +1,5 @@
+package com.henriquebarucco.baxoapi.domain.user.interactor
+
+interface CreateUserInteractor {
+    fun execute()
+}
